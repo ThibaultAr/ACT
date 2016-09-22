35617d1de63ce877c6e753621c62b1ef0aebf0f5
Yann Dubois <ttreplay@gmail.com>
2016-09-22 14:18:30 +0200

    Base of the second algo

--- TP1/tp1.c
+++ TP1/tp1.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 struct point_s {
-  unsigned long int x;
-  unsigned long int y;
+  int x;
+  int y;
 };
 
 typedef struct point_s point;
@@ -12,20 +12,67 @@ typedef struct point_s point;
   h -> hauteur
   n -> nombre de points
   points -> tableau de points
+  surface -> adresse d'un entier pour afficher la surface dans le main
+  point1 -> adresse d'un point pour l'afficher dans le main
+  point2 -> adresse d'un point pour l'afficher dans le main
 */
-void res (int l, int h, int n, point* points){
+void resDivide(int l, int h, int n, point* points, int* surface, point* point1, point* point2) {
+  if(n == 2) {
+    (*surface) = l * h;
+    point1->x = points[0].x;
+    point1->y = 0;
+    point2->x = points[1].x;
+    point2->y = h;
+  }
+  else {
+    int minOrd = h;
+    int indicePoint, left, right;
+    point pointLeft1, pointLeft2, pointRight1, pointRight2;
+
+    for(int i = 1; i < n - 1; i++) {
+      if(points[i].y < minOrd) {
+        minOrd = points[i].y;
+        indicePoint = i;
+      }
+    }
+
+    resDivide(points[indicePoint].x, h, indicePoint + 1, points, &left, &pointLeft1, &pointLeft2);
+    resDivide(l - points[indicePoint].x, h, n - indicePoint, points + indicePoint, &right, &pointRight1, &pointRight2);
+
+    printf("left: %d\n", left);
+    printf("right: %d\n", right);
+
+    if(left < right) {
+      (*surface) = right;
+      (*point1) = pointRight1;
+      (*point2) = pointRight2;
+    }
+    else {
+      (*surface) = left;
+      (*point1) = pointLeft1;
+      (*point2) = pointLeft2;
+    }
+  }
+}
+
+/*l -> largeur
+  h -> hauteur
+  n -> nombre de points
+  points -> tableau de points
+*/
+void res (unsigned long int l, unsigned long int h, unsigned long int n, point* points){
   point actual;
   point point1, point2;
-  unsigned long int surface = 0;
+  int surface = 0;
 
   /* Pour i allant de 0 à n - 2 (du début jusque l'avant dernier) */
   for(int i = 0; i < n - 1; i++){
-    unsigned long int minOrd = h;
+    int minOrd = h;
     actual = points[i];
 
     /* Pour j allant de i + 1 (voisin direct à droite) à n - 1 (tout les voisin de droites) */
     for(int j = i + 1; j < n; j++){
-      unsigned long int surfaceTMP;
+      int surfaceTMP;
 
       surfaceTMP = (points[j].x - points[i].x) * minOrd;
       if(surface < surfaceTMP) {
@@ -41,18 +88,18 @@ void res (int l, int h, int n, point* points){
     }
   }
 
-  printf("surface: %lu, p1: (%lu, %lu), p2: (%lu, %lu)", surface, point1.x, point1.y, point2.x, point2.y);
+  printf("surface: %d, p1: (%d, %d), p2: (%d, %d)", surface, point1.x, point1.y, point2.x, point2.y);
 }
 
 int main (char* argv[], int argc) {
-  unsigned long int n, l, h, i;
+  int n, l, h, i, surface;
   point* points;
-  point begin, end;
+  point begin, end, p1, p2;
 
   i = 0;
 
-  scanf("%lu %lu", &l, &h);
-  scanf("%lu", &n);
+  scanf("%d %d", &l, &h);
+  scanf("%d", &n);
 
   points = malloc((n + 2) * sizeof(point));
   begin.x = 0;
@@ -61,7 +108,7 @@ int main (char* argv[], int argc) {
 
   for(;i < n + 1; i++) {
     point actual;
-    scanf("%lu %lu", &actual.x, &actual.y);
+    scanf("%d %d", &actual.x, &actual.y);
     points[i] = actual;
   }
 
@@ -69,7 +116,10 @@ int main (char* argv[], int argc) {
   end.y = 0;
   points[n + 1] = end;
 
-  res(l, h, n + 2, points);
+  /*res(l, h, n + 2, points);*/
+
+  resDivide(l, h, n + 2, points, &surface, &p1, &p2);
+  printf("surface: %d, p1: (%d, %d), p2: (%d, %d)", surface, p1.x, p1.y, p2.x, p2.y);
 
   free(points);
 
