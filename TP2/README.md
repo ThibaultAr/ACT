968d14159f40d62207848763eb3ec77083b2f1d6
Yann Dubois <ttreplay@gmail.com>
2016-10-15 15:12:09 +0200

    Move to ponderation

--- TP2/README.md
+++ TP2/README.md
@@ -5,7 +5,14 @@ langage: C
 
 Question 1:
 
+meilleur gris :
+
 $ ./tp2
 10
 148
 255
+
+distanceMin :
+
+$ ./tp2
+5096
--- TP2/tp2.c
+++ TP2/tp2.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
-int meilleurGris(int indicePremier, int indiceDernier, int * tab) {
-  int res = 0;
+struct palette_s {
+  int *pixels;
+  int *weight;
+};
 
-  for(int i = indicePremier; i <= indiceDernier; i++)
-    res += tab[i];
+int meilleurGris(int indicePremier, int indiceDernier, struct palette_s palette) {
+  int res = 0, totalWeight = 0;
 
-  return ((float)res / (float)(indiceDernier - indicePremier + 1)) + 0.5;
+  for(int i = indicePremier; i <= indiceDernier; i++) {
+    res += palette.pixels[i] * palette.weight[i];
+    totalWeight += palette.weight[i];
+  }
+
+  return (float)res / (float)(totalWeight);
 }
 
-int distanceMin (int indicePremier, int indiceDernier, int * tab) {
-  int meilleurGrisLoc = meilleurGris(indicePremier, indiceDernier, tab);
+int distanceMin (int indicePremier, int indiceDernier, struct palette_s palette) {
+  int meilleurGrisLoc = meilleurGris(indicePremier, indiceDernier, palette);
   int res = 0;
 
   for(int i = indicePremier; i <= indiceDernier; i++)
-    res += (meilleurGrisLoc - tab[i]) * (meilleurGrisLoc - tab[i]);
+    res += (meilleurGrisLoc - palette.pixels[i]) * (meilleurGrisLoc - palette.pixels[i]) * palette.weight[i];
 
   return res;
 }
@@ -23,10 +30,17 @@ int distanceMin (int indicePremier, int indiceDernier, int * tab) {
 int main(){
 
   int tab[25] = {0,0,0,0,0,20,20,20,20,20,100,132,164,164,180,255,255,255,255,255,255,255,255,255,255};
-  /* TEST QUESTION 1.1
-  printf("%d\n%d\n%d\n", meilleurGris(0, 9,tab),meilleurGris(10, 14,tab), meilleurGris(15, 24,tab));
-  */
+
+  int pixels[7] = {0,20,100,132,164,180,255};
+  int weight[7] = {5,5,1,1,2,1,10};
+
+  struct palette_s palette;
+  palette.pixels = pixels;
+  palette.weight = weight;
+  /* TEST QUESTION 1.1*/
+  /*printf("%d\n%d\n%d\n", meilleurGris(0, 1,palette),meilleurGris(2, 5,palette), meilleurGris(6, 7,palette));*/
+
   /*TEST QUESTION 1.2*/
-  printf("%d\n", distanceMin(0, 9,tab) + distanceMin(10, 14,tab) + distanceMin(15, 24,tab));
+  printf("%d\n", distanceMin(0, 1,palette) + distanceMin(2, 5,palette) + distanceMin(6, 7,palette));
   return 0;
 }
