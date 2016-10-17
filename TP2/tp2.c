27ab9fac623d753c71618887e04a4be5121a5201
Thibault Arloing <thibault.arloing@hotmail.fr>
2016-10-17 16:47:11 +0200

    Q4

--- TP2/tp2.c
+++ TP2/tp2.c
@@ -27,6 +27,15 @@ int distanceMin (int indicePremier, int indiceDernier, struct palette_s palette)
   return res;
 }
 
+int totalPoid (int indicePremier, int indiceDernier, struct palette_s palette) {
+  int total = 0;
+
+  for(int i = indicePremier; i <= indiceDernier; i ++)
+    total += palette.weight[i];
+
+  return total;
+}
+
 int reduction_naive(int k, struct palette_s palette, int n) {
   if(k == 1) {
     return distanceMin(0, n - 1, palette);
@@ -50,7 +59,7 @@ int reduction_naive(int k, struct palette_s palette, int n) {
   }
 }
 
-int reduction(int k, struct palette_s palette, int n) {
+int reduction(int k, struct palette_s palette, int n, struct palette_s * new_palette) {
   int *tab = malloc(k * n * sizeof(int));
   //int *dst = malloc(n * n * sizeof(int));
   int *dec = malloc(k * n * sizeof(int));
@@ -77,7 +86,20 @@ int reduction(int k, struct palette_s palette, int n) {
     }
   }
 
-  /* Rende arrays */
+  new_palette->pixels = malloc(k * sizeof(int));
+  new_palette->weight = malloc(k * sizeof(int));
+
+  int i = 0;
+  for(int j = k - 1; j > 0; j--){
+    new_palette->pixels[k - j - 1] = meilleurGris(i, dec[i + j * n]-1, palette);
+    new_palette->weight[k - j - 1] = totalPoid(i, dec[i + j * n]-1, palette);
+    i = dec[i + j * n];
+  }
+
+  new_palette->pixels[k - 1] = meilleurGris(i, n, palette);
+  new_palette->weight[k - 1] = totalPoid(i, n, palette);
+
+  /* Render of arrays */
 
 /*
   for(int i = 0; i < k; i++) {
@@ -105,13 +127,49 @@ int reduction(int k, struct palette_s palette, int n) {
   return res;
 }
 
+void transImage(int pixels[], int n, int k) {
+  int weight[256];
+  int nbColors = 0, acc = 0;
+  struct palette_s palette, new_palette;
+
+  for(int i = 0; i < 256; i++)
+    weight[i] = 0;
+
+  for(int i = 0; i < n; i++){
+    weight[pixels[i]]++;
+    if(weight[pixels[i]] == 1)
+      nbColors++;
+  }
+
+  palette.pixels = malloc(nbColors * sizeof(int));
+  palette.weight = malloc(nbColors * sizeof(int));
+
+  for(int i = 0; i < 256; i++){
+    if(weight[i]) {
+      palette.pixels[acc] = i;
+      palette.weight[acc] = weight[i];
+      acc++;
+    }
+  }
+
+  reduction(k, palette, nbColors, &new_palette);
+
+  printf("%d\n", new_palette.pixels[1]);
+
+}
+
 int main(){
+
+  int pixels[25] = {0,0,0,0,0,20,20,20,20,20,100,132,164,164,180,255,255,255,255,255,255,255,255,255,255};
+/*
   int pixels[7] = {0,20,100,132,164,180,255};
   int weight[7] = {5,5,1,1,2,1,10};
 
   struct palette_s palette;
   palette.pixels = pixels;
   palette.weight = weight;
+*/
+
   /* TEST QUESTION 1.1*/
   /*printf("%d\n%d\n%d\n", meilleurGris(0, 1,palette),meilleurGris(2, 5,palette), meilleurGris(6, 6,palette));*/
 
@@ -121,7 +179,7 @@ int main(){
   /*TEST QUESTION 2*/
   /*printf("%d\n", reduction_naive(3, palette, 7));*/
 
-  printf("%d\n", reduction(3, palette, 7));
+  transImage(pixels, 25, 3);
 
   return 0;
 }
