1d3b9c36eb93fb1b3c1948d28e35f009f782d2cb
Thibault Arloing <thibault.arloing@hotmail.fr>
2016-10-23 17:26:05 +0200

    finish test.sh

--- TP2/test.sh
+++ TP2/test.sh
@@ -2,10 +2,14 @@ if [ ! -e images-test/baboon_gray.pgm ]; then
   convert -depth 8 -size 512x512 -compress none images-test/baboon_gray.png images-test/baboon_gray.pgm
 fi
 
+sed -i".bak" '/#/d' images-test/baboon_gray.pgm > /dev/null
+rm images-test/*.bak
+
 echo "*****************************"
 echo "Test sur Baboon_gray.pgm"
 echo ""
-make exec FILE=images-test/baboon_gray.pgm COLORS=3
+make exec FILE=images-test/baboon_gray.pgm COLORS=3 > images-test/baboon_result.pgm
+echo "make exec FILE=images-test/baboon_gray.pgm COLORS=3 > images-test/baboon_result.pgm"
 
 echo -e "\n\n*****************************"
 echo "Test sur 1pix.pgm"
