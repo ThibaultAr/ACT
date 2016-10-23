cf63f1fae1ed81c42d8c0f93412b24682f7cfdf7
Thibault Arloing <thibault.arloing@hotmail.fr>
2016-10-23 16:37:17 +0200

    test TP1

---
+++ TP1/test.sh
@@ -0,0 +1,58 @@
+echo "*****************************"
+echo "Tests sur ex=100000_1000000"
+echo ""
+make exec FILE=don/donneesPourTests/ex=100000_1000000
+
+echo ""
+echo "*****************************"
+echo "Tests sur ex=200000_2000000"
+echo ""
+make exec FILE=don/donneesPourTests/ex=200000_2000000
+
+echo ""
+echo "*****************************"
+echo "Tests sur ex0point_10000"
+echo ""
+make exec FILE=don/donneesPourTests/ex0point_10000
+
+echo ""
+echo "*****************************"
+echo "Tests sur ex10_24400144"
+echo ""
+make exec FILE=don/donneesPourTests/ex10_24400144
+
+echo ""
+echo "*****************************"
+echo "Tests sur ex100_5980"
+echo ""
+make exec FILE=don/donneesPourTests/ex100_5980
+
+echo ""
+echo "*****************************"
+echo "Tests sur ex500_7616"
+echo ""
+make exec FILE=don/donneesPourTests/ex500_7616
+
+echo ""
+echo "*****************************"
+echo "Tests sur ex100000_100000"
+echo ""
+make exec FILE=don/donneesPourTests/ex100000_100000
+
+echo ""
+echo "*****************************"
+echo "Tests sur exCodeChef_49997500"
+echo ""
+make exec FILE=don/donneesPourTests/exCodeChef_49997500
+
+echo ""
+echo "*****************************"
+echo "Tests sur exT100000_30011389"
+echo ""
+make exec FILE=don/donneesPourTests/exT100000_30011389
+
+echo ""
+echo "*****************************"
+echo "Tests sur exT200000_75141975"
+echo ""
+make exec FILE=don/donneesPourTests/exT200000_75141975
