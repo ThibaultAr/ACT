774e0381dbc6bf0ce70df27e9c8441efdb72fbef
Yann Dubois <ttreplay@gmail.com>
2016-12-01 21:03:50 +0100

    Random algo, still a bug in verification

--- TP4/HashCode/src/Main.java
+++ TP4/HashCode/src/Main.java
@@ -1,13 +1,13 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 	
-	private List<Integer[]> parts = new ArrayList<Integer[]>();
+	private List<int[]> parts = new ArrayList<int[]>();
 	
 	private void generateAllParts(char[][] pizza) {
 		//On sélectionne une cellule précise
@@ -16,7 +16,7 @@ public class Main {
 				//Générer toute les parts possibles à partir de la celllule
 				for(int k = i; k < pizza.length; k++) {
 					for(int l = j; l < pizza[0].length; l++) {
-						Integer[] partsArray = new Integer[4];
+						int[] partsArray = new int[4];
 						partsArray[0] = i;
 						partsArray[1] = j;
 						partsArray[2] = l;
@@ -25,6 +25,37 @@ public class Main {
 					}
 				}
 			}
+		}
+	}
+	
+	private void randomSolution(int nbJambon, int taillePart, char[][] pizza) {
+		List<int[]> solve = new ArrayList<int[]>();
+		List<int[]> partsCopy = new ArrayList<int[]>(parts);
+		Random random = new Random();
+		int[][] certif = null;
+		
+		while(!partsCopy.isEmpty()) {
+			int index = random.nextInt(partsCopy.size());
+			int[] part = partsCopy.get(index);
+			int[][] certifTmp = new int[solve.size()][4];
+			
+			solve.add(part);
+			partsCopy.remove(index);
+			solve.toArray(certifTmp);
+			CertificatPizza c = new CertificatPizza(certifTmp, nbJambon, taillePart);
+			if(!c.verif(pizza)) {
+				solve.remove(part);
+			}
+			certif = certifTmp;
+		}
+		
+		this.printSolution(certif);
+	}
+	
+	private void printSolution(int[][] solve) {
+		System.out.println(solve.length);
+		for(int i = 0; i < solve.length; i++) {
+			System.out.println(solve[i][0] + " " + solve[i][1] + " " + solve[i][2] + " " + solve[i][3]);
 		}
 	}
 	
@@ -59,6 +90,7 @@ public class Main {
 			
 			Main main = new Main();
 			main.generateAllParts(pizza);
+			main.randomSolution(nbJambon, taillePart, pizza);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
